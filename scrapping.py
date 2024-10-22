2f8d5b20d0746c6aa3921e5419cfdc2a996398ed
Pierric Letard <pierric@radiomail-fr.com>
2024-10-22 15:54:17 +0200

    books from a category

--- scrapping.py
+++ scrapping.py
@@ -30,40 +30,42 @@ def get_int_from_string(string):
     return number
 
 site = get_soup(site_url)
-
+category_url = 'https://books.toscrape.com/catalogue/category/books/classics_6/index.html'
+category_page = get_soup(category_url)
 
 all_products= []
 
+for product in  category_page.find_all('h3'):
+    product_url = product.find('a')['href']
+    product_page = get_soup('https://books.toscrape.com/catalogue/' + product_url[9:])
 
-product_page = get_soup('https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html')
+    product_table = product_page.find('table')
+    universal_product_code = product_table.find_all('td')[0].text
+    product_title = product_page.find('h1').text
 
-universal_product_code = product_page.find('table').find_all('td')[0].text
-product_title = product_page.find('h1').text
+    product_including_tax = product_table.find_all('td')[3].text
+    product_excluding_tax = product_table.find_all('td')[2].text
 
-product_including_tax = product_page.find('table').find_all('td')[3].text
-product_excluding_tax = product_page.find('table').find_all('td')[2].text
+    product_number_available = get_int_from_string(product_table.find_all('td')[5].text)
 
-product_number_available_string = product_page.find('table').find_all('td')[5].text
-product_number_available = get_int_from_string(product_number_available_string)
+    product_description = product_page.find_all('p')[3].text
+    product_category = product_page.find_all('a')[3].text
+    product_review_rating = product_page.find('p', class_='star-rating')['class'][1]
+    product_image_url = product_page.find('img')['src']
 
-product_description = product_page.find_all('p')[3].text
-product_category = product_page.find_all('a')[3].text
-product_review_rating = product_page.find('p', class_='star-rating')['class'][1]
-product_image_url = product_page.find('img')['src']
+    all_products.append({
+        'universal_product_code': universal_product_code,
+        'product_title': product_title,
+        'product_including_tax': product_including_tax,
+        'product_excluding_tax': product_excluding_tax,
+        'product_number_available': product_number_available,
+        'product_description': product_description,
+        'product_category': product_category,
+        'product_review_rating': product_review_rating,
+        'product_image_url': product_image_url
+    })
 
-all_products.append({
-    'universal_product_code': universal_product_code,
-    'product_title': product_title,
-    'product_including_tax': product_including_tax,
-    'product_excluding_tax': product_excluding_tax,
-    'product_number_available': product_number_available,
-    'product_description': product_description,
-    'product_category': product_category,
-    'product_review_rating': product_review_rating,
-    'product_image_url': product_image_url
-})
-
-with open('products.csv', 'w', newline='') as fichier_csv:
+with open('products.csv', 'w', newline='', encoding='utf-8') as fichier_csv:
     writter = csv.writer(fichier_csv)
     writter.writerow(['universal_product_code', 'product_title', 'product_including_tax', 'product_excluding_tax', 'product_number_available', 'product_description', 'product_category', 'product_review_rating', 'product_image_url'])
     for product in all_products:
