58e962df2517beea273517acf48770493b616170
Pierric Letard <pierric@radiomail-fr.com>
2024-10-23 10:39:14 +0200

    all category

--- scrapping.py
+++ scrapping.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import os
+import shutil
 
 site_url = 'https://books.toscrape.com/'
 
@@ -31,18 +32,18 @@ def get_int_from_string(string):
     return number
 
 
-def category_explorer(url):
+def category_explorer(url, category_products):
     category_page = get_soup(url)
-    page_explorer(category_page)
+    page_explorer(category_page, category_products)
     if category_page.find('li', class_='next'):
         next_page_url = category_page.find('li', class_='next').find('a')['href']
-        print('on passe à la page suivante', next_page_url)
-        category_explorer(category_url + next_page_url)
+        print('on passe à la page suivante', category_url + next_page_url)
+        category_explorer(category_url + next_page_url, category_products)
     else:
         print('fini')
-    return None
+    return category_products
 
-def page_explorer(page):
+def page_explorer(page, category_products):
     for product in page.find_all('h3'):
         product_url = product.find('a')['href'].replace('../', '')
         product_page = get_soup('https://books.toscrape.com/catalogue/' + product_url)
@@ -63,7 +64,7 @@ def page_explorer(page):
         product_image_url = product_page.find('img')['src']
         product_image_url = product_image_url.replace('../', '')
         product_image_url = 'https://books.toscrape.com/' + product_image_url
-        all_products.append({
+        category_products.append({
             'universal_product_code': universal_product_code,
             'product_title': product_title,
             'product_including_tax': product_including_tax,
@@ -85,30 +86,43 @@ def clean_name(nom):
     return nom
 
 
-def download_image(url_image, nom_image):
+def download_image(url_image, nom_image, category):
     response = requests.get(url_image)
     nom_image = clean_name(nom_image)
-    chemin_image = os.path.join('dossier_images', nom_image + '.jpg')
-    
-    print(chemin_image)
+    nom_dossier = clean_name(category)
+    chemin_image = os.path.join('dossier_images', nom_dossier, nom_image + '.jpg')    
     with open(chemin_image, 'wb') as fichier:
         fichier.write(response.content)
     print(f"Image {nom_image} téléchargée avec succès!")
-    
-site = get_soup(site_url)
-category_url = 'https://books.toscrape.com/catalogue/category/books/sequential-art_5/'
+    return None
 
-all_products= []
+def file_writer(category_name, products):  
+    folder_maker('dossier_csv/' + category_name)
+    folder_maker('dossier_images/' + category_name)
+    chemin_csv = os.path.join('dossier_csv', category_name, category_name + '.csv')
+    with open(chemin_csv, 'w', newline='', encoding='utf-8-sig') as fichier_csv:
+        writter = csv.writer(fichier_csv, delimiter=';')
+        writter.writerow(['universal_product_code', 'product_title', 'product_including_tax', 'product_excluding_tax', 'product_number_available', 'product_description', 'product_category', 'product_review_rating', 'product_image_url'])
+        for product in products:
+            download_image(product['product_image_url'], product['product_title'], product['product_category'])
+            writter.writerow([product['universal_product_code'], product['product_title'], product['product_including_tax'], product['product_excluding_tax'], product['product_number_available'], product['product_description'], product['product_category'], product['product_review_rating'], product['product_image_url']])
+    return None
 
-category_explorer(category_url)
+def site_explorer():
+    global category_url
+    site = get_soup(site_url)
+    for category in site.find('ul', class_='nav nav-list').find('ul').find_all('a'):
+        category_name = category.text.strip()
+        category_url = site_url + category['href'].replace('index.html', '')
+        category_products = category_explorer(category_url, [])
+        file_writer(category_name, category_products)
+    return None   
 
-if not os.path.exists('dossier_images'):
-    os.makedirs('dossier_images')
-    
-with open('products.csv', 'w', newline='', encoding='utf-8-sig') as fichier_csv:
-    writter = csv.writer(fichier_csv, delimiter=';')
-    writter.writerow(['universal_product_code', 'product_title', 'product_including_tax', 'product_excluding_tax', 'product_number_available', 'product_description', 'product_category', 'product_review_rating', 'product_image_url'])
-    for product in all_products:
-        print(product['product_image_url'])
-        download_image(product['product_image_url'], product['product_title'])
-        writter.writerow([product['universal_product_code'], product['product_title'], product['product_including_tax'], product['product_excluding_tax'], product['product_number_available'], product['product_description'], product['product_category'], product['product_review_rating'], product['product_image_url']])
+def folder_maker(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+category_url = ''
+folder_maker('dossier_images')
+folder_maker('dossier_csv')
+site_explorer()
